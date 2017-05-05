cd2d6118238c923a8431b911626da89b95ee9e72
witekbobrowski <witek@bobrowski.com.pl>
2017-05-05 09:25:50 +0200

    Day 1: Data Types

---
+++ 30 Days of Code/day-1-data-types.swift
@@ -0,0 +1,13 @@
+import Foundation
+
+let integer: Int = Int(readLine()!)!
+let double: Double = Double(readLine()!)!
+let string: String =  readLine()!
+
+var i = 4
+var d = 4.0
+var s = "HackerRank "
+
+print(i + integer)
+print(d + double)
+print(s + string)

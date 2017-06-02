881953d73b5b494e8543642ccc4d500efb43ea6a
witekbobrowski <witek@bobrowski.com.pl>
2017-06-02 13:56:57 +0200

    Supplied the Swift solution

--- 30 Days of Code/day-13-abstract-classes.swift
+++ 30 Days of Code/day-13-abstract-classes.swift
@@ -1,22 +1,54 @@
 //
 //  So... Swift does not support abstract classes so I had to do this challenge in c++.
-//  Thats it for the 'Swift only' solutions! Thanks HackerRank <3
+//
+//     class MyBook: Book{
+//
+//          public:
+//              int price;
+//              MyBook(string t, string a, int p): Book(t, a){
+//                  price = p;
+//              }
+//          void display(){
+//              std::cout << "Title: " << title << endl << "Author: " << author << endl << "Price: " << price;
+//          }
+//
+//      };
+//
+//  But here is the Swift solution:
 //
 
-class MyBook: Book{
+protocol Book {
 
-    public:
-        int price;
-        MyBook(string t, string a, int p): Book(t, a){
-            price = p;
-        }
+    var title: String { get set }
+    var author: String { get set }
 
-    void display(){
-        std::cout << "Title: " << title << endl << "Author: " << author << endl << "Price: " << price;
+    init(title: String, author: String)
+
+    func display()
+
+}
+
+class MyBook: Book {
+
+    var title: String
+    var author: String
+    var price: Int?
+
+    required init(title: String, author: String) {
+        self.title = title
+        self.author = author
     }
 
-};
+    convenience init(title: String, author: String, price: Int) {
+        self.init(title: title, author: author)
+        self.price = price
+    }
 
-//
-//  P.S. My heart is broken... ;_; [*]
-//
+    func display() {
+        print("Title: \(title)\nAuthor: \(author)\nPrice: \(price ?? 0 )")
+    }
+
+}
+
+let book = MyBook(title: readLine()!, author: readLine()!, price: Int(readLine()!)!)
+book.display()
